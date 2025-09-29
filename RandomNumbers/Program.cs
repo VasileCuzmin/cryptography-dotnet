aa32d902d265a6242256a93c223bfe4ac56b4785
vcuzmin <cuzmin@hlsr.com>
2025-09-29 13:22:42 +0300

    random numbers generator

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+for (var i = 0; i < 10; i++)
+{
+    var randomNumbers = RandomNumberGenerator.GetBytes(32);
+    Console.WriteLine("Random number " + i + " : " + Convert.ToBase64String(randomNumbers));
+}
+
+Console.ReadLine();
